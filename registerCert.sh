b87b53d9c24ddf60b92878e43546b25409f3420c
vyrwu <alek_nowak@hotmail.com>
2019-01-04 17:37:57 +0100

    Tweak nginx

--- registerCert.sh
+++ registerCert.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+cd /var/lib/iafoosball/cert
+CERT=$(cat fullchain.pem)
+KEY=$(cat privkey.pem)
+
 curl -i -X POST http://localhost:8050/certificates \
--F "cert=@/var/lib/iafoosball/cert/fullchain.pem" \
--F "key=@/var/lib/iafoosball/cert/privkey.pem" \
--F "snis="
+--data "cert=$CERT" \
+--data "key=$KEY" \
+--data 'snis[]=iafoosball.me'
+
+#curl -i -X POST http://localhost:8050/certificates \
+#    -F "cert=@/var/lib/iafoosball/cert/fullchain.pem" \
+#    -F "key=@/var/lib/iafoosball/cert/privkey.pem" \
+#    -F "snis=iafoosball.me"
+
