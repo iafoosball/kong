2e8fcfda459fa7b931bf2318e496bd12404ee6d0
vyrwu <alek_nowak@hotmail.com>
2019-01-20 13:48:56 +0100

    Add https certificates

--- registerCert.sh
+++ registerCert.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-cd /var/lib/iafoosball/cert
-CERT=`cat fullchain.pem`
-KEY=`cat privkey.pem`
-
-echo $CERT
-
-curl -i -X POST http://localhost:8051/certificates \
---data "cert=$CERT" \
---data "key=$KEY" \
---data 'snis[]=iafoosball.me'
-
-#curl -i -X POST http://localhost:8051/certificates \
-#    -F "cert=@/var/lib/iafoosball/cert/fullchain.pem" \
-#    -F "key=@/var/lib/iafoosball/cert/privkey.pem" \
-#    -F "snis=iafoosball.me"
-
+curl -i -X POST http://localhost:8051/certificates/ \
+  -H 'Content-Type: application/json' \
+  -d "{\"cert\": \"$(cat /var/lib/iafoosball/cert/fullchain.pem)\", \"key\": \"$(cat /var/lib/iafoosball/cert/privkey.pem)\", \"snis\": [ \"iafoosball.me\" ] }"
